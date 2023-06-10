4eb9a5d6b64e101dce1eb0a8c2f5359527ba2b8b
michellebessa <michelleandressa12@gmail.com>
2023-06-10 00:07:00 +0000

    changes

--- src/app.js
+++ src/app.js
@@ -1,43 +1,36 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
+const suit = ["♥", "♠", "♣", "♦"];
+const numbers = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+export default function randomCard() {
+  const randomSuit = suit[Math.floor(Math.random() * suit.length)];
+  const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+
+  document.querySelector(".header").innerHTML = randomSuit;
+  document.querySelector(".body").innerHTML = randomNumber;
+  document.querySelector(".footer").innerHTML = randomSuit;
+
+  if (randomSuit === "♥" || randomSuit === "♦") {
+    document.querySelector("#cardcontainer").style.color = "red";
+  } else if (randomSuit === "♠" || randomSuit === "♣") {
+    document.querySelector("#cardcontainer").style.color = "black";
+  }
+}
 
 window.onload = function() {
-  let suit = ["♥", "♠", "♣", "♦"];
-  let numbers = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
-
-  function randomCard() {
-    let randomSuit = suit[Math.floor(Math.random() * suit.length)];
-    let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
-
-    document.querySelector(".header").innerHTML = randomSuit;
-    document.querySelector(".body").innerHTML = randomNumber;
-    document.querySelector(".footer").innerHTML = randomSuit;
-
-    if (randomSuit == "♥" || randomSuit == "♦") {
-      document.querySelector("#cardcontainer").style.color = "red";
-    } else if (randomSuit == "♠" || randomSuit == "♣") {
-      document.querySelector("#cardcontainer").style.color = "black";
-    }
-  }
-
   randomCard();
 };
-window.onload();
